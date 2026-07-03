d3a74dc72865a0cf054b4014b32bc9c201fbf87a
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-07-03 13:07:17 +0200

    feat: add day3 header file with all protos

--- day3/day3.h
+++ day3/day3.h
@@ -10,10 +10,19 @@
 
     /* MACRO */
     #define NB_LETTERS 26
+    #define MAX_DIGITS 10
+    #define FALSE 0
+    #define TRUE 1
 
 /* PROTO */
 void my_putchar(char c);
+
 int my_print_alpha(void);
 int my_print_revalpha(void);
+int my_print_digits(void);
+int my_isneg(int n);
+int my_print_comb(void);
+int my_print_comb2(void);
+int my_put_nbr(int nb);
 
 #endif /* !DAY3_H */
