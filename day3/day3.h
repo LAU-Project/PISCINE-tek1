33f0bb5d22b8dc174903d067f7d450ba2ed5236d
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-07-03 11:12:21 +0200

    feat: add day3 header file into day3

---
+++ day3/day3.h
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2026
+** day3
+** File description:
+** day3
+*/
+
+#ifndef DAY3_H
+    #define DAY3_H
+
+    /* MACRO */
+    #define NB_LETTERS 26
+
+/* PROTO */
+void my_putchar(char c);
+int my_print_alpha(void);
+int my_print_revalpha(void);
+
+#endif /* !DAY3_H */
