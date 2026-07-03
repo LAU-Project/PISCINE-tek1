f8fdbeec618764f53cbcb507c860b1579dbd5dc0
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-07-03 12:22:05 +0200

    feat: add my print comb into day3

---
+++ day3/my_print_comb.c
@@ -0,0 +1,45 @@
+/*
+** EPITECH PROJECT, 2026
+** print comb
+** File description:
+** print comb
+*/
+
+#include "day3.h"
+
+static void print_comb(int a, int b, int c)
+{
+    my_putchar('0' + a);
+    my_putchar('0' + b);
+    my_putchar('0' + c);
+}
+
+static void print_sep(void)
+{
+    my_putchar(',');
+    my_putchar(' ');
+}
+
+int my_print_comb(void)
+{
+    int a = 0;
+    int b = 1;
+    int c = 2;
+
+    while (1) {
+        print_comb(a, b, c);
+        c += 1;
+        if (c >= 9) {
+            b += 1;
+            c = b + 1;
+        }
+        if (b >= 8) {
+            a += 1;
+            b = a + 1;
+        }
+        if (a >= 8)
+            break;
+        print_sep();
+    }
+    return 0;
+}
