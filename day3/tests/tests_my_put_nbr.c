cd54817eb7a5f621de935b61156f405ca6091e21
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-07-03 13:06:58 +0200

    test: add unit test for put nbr into day3

---
+++ day3/tests/tests_my_put_nbr.c
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2026
+** unit test
+** File description:
+** unit test
+*/
+
+#include "../day3.h"
+
+int main(void)
+{
+    my_put_nbr(0);
+    my_put_nbr(42);
+    my_put_nbr(-42);
+    my_put_nbr(2147483647);
+    my_put_nbr(-2147483648);
+    return 0;
+}
