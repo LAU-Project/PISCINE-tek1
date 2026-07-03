7da7cd07c1f5d593f6aeb3e27da690cf5d6ab2a8
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-07-03 13:06:34 +0200

    fix: my print comb

--- day3/my_print_comb.c
+++ day3/my_print_comb.c
@@ -20,6 +20,19 @@ static void print_sep(void)
     my_putchar(' ');
 }
 
+static void update_comb(int *a, int *b, int *c)
+{
+    if (*c > 9) {
+        *b += 1;
+        *c = *b + 1;
+        if (*b > 8) {
+            *a += 1;
+            *b = *a + 1;
+            *c = *b + 1;
+        }
+    }
+}
+
 int my_print_comb(void)
 {
     int a = 0;
@@ -29,14 +42,7 @@ int my_print_comb(void)
     while (1) {
         print_comb(a, b, c);
         c += 1;
-        if (c >= 9) {
-            b += 1;
-            c = b + 1;
-        }
-        if (b >= 8) {
-            a += 1;
-            b = a + 1;
-        }
+        update_comb(&a, &b, &c);
         if (a >= 8)
             break;
         print_sep();
