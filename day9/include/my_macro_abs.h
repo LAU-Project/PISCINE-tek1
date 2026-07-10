e3e46dd7bff803013c3f4261036a9d8e982ef8d7
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-07-10 10:36:16 +0200

    feat: add my_macro abs heaferfile into day9

---
+++ day9/include/my_macro_abs.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2026
+** my macro
+** File description:
+** my macro
+*/
+
+#ifndef MY_MACRO_ABS_H
+    #define MY_MACRO_ABS_H
+
+    #define ABS(value) (((value) < 0) ? -(value) : (value))
+
+#endif /* !MY_MACRO_ABS_H */
